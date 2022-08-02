098f587821d6174f09da362c152a9223a49d427c
lucas.fraga <lucas.fraga@bnex.com.br>
2022-08-02 14:14:34 -0300

    test 3

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = {
     video: false,
     e2e: {
         baseUrl: "https://serverest.dev",
+        email: "fulano@qa.com",
+        password: "teste",
         setupNodeEvents(on, config) {
             allureWriter(on, config);
             return config;
