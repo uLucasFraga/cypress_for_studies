68795e7318079dba6b2b6bc3567563dbb859c08d
lucas.fraga <lucas.fraga@bnex.com.br>
2022-08-02 13:40:55 -0300

    up baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = {
     video: false,
     e2e: {
+        baseUrl: Cypress.env("baseUrl"),
         setupNodeEvents(on, config) {
             allureWriter(on, config);
             return config;
